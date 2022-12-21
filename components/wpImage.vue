036e1e07ce6f8054a3305b832ebef9c257de6111
simonproudfoot <simonproudfoot1980@gmail.com>
2022-12-21 20:45:30 +0000

    removed legacy js

--- components/wpImage.vue
+++ components/wpImage.vue
@@ -1,5 +1,5 @@
 <template>
-<img  @load="$emit('imageLoaded')" :src="fullSize" :sizes="`(max-width: 600px) ${srcset.sizes}`" :srcset="srcset.imgSrc" :alt="$store.state.page.pageFeatures.featuredImage.alt" :class="classList" />
+<img @load="$emit('imageLoaded')" :src="fullSize" :sizes="`(max-width: 600px) ${srcset.sizes}`" :srcset="srcset.imgSrc" :alt="$store.state.page.pageFeatures.featuredImage.alt" :class="classList" />
 </template>
 
 <script>
@@ -7,14 +7,15 @@ export default {
     props: ['classList'],
     computed: {
         fullSize() {
-            const img = Object.values(this.$store.state.page.pageFeatures.featuredImage.sizes)
+
+            const img = Object.keys(this.$store.state.page.pageFeatures.featuredImage.sizes).map(e => this.$store.state.page.pageFeatures.featuredImage.sizes[e])
             const size = Object.keys(img).length - 1
             return img[size].source_url
         },
         srcset() {
             let srcsetValues = []
             let sizes = []
-            const img = Object.values(this.$store.state.page.pageFeatures.featuredImage.sizes)
+            const img = Object.keys(this.$store.state.page.pageFeatures.featuredImage.sizes).map(e => this.$store.state.page.pageFeatures.featuredImage.sizes[e])
             img.forEach(size => {
                 srcsetValues.push(size.source_url + ' ' + size.width + 'w')
                 sizes.push(size.width + 'px')
