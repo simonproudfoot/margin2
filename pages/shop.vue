59690c65f2df1ac81c4e533abee761e675d63906
simonproudfoot <simonproudfoot1980@gmail.com>
2022-12-27 12:07:36 +0000

    env issue fixed

--- pages/shop.vue
+++ pages/shop.vue
@@ -76,7 +76,7 @@ export default {
     async asyncData({ params, $axios, store }) {
         try {
             const response = await $axios.get(
-                `https://remarkable-arithmetic-20e4c5.netlify.app/.netlify/functions/wooProducts`
+                `${process.env.netlifyFunctionsUrl}/wooProducts`
             );
             const products = await response.data;
             return {
