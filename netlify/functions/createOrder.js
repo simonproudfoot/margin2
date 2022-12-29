8070797f03729ae9aff658e281befd5542d2fb11
simonproudfoot <simonproudfoot1980@gmail.com>
2022-12-29 12:13:38 +0000

    create order netlify function

--- netlify/functions/createOrder.js
+++ netlify/functions/createOrder.js
@@ -1,72 +1,9 @@
 const axios = require('axios');
 const https = require('https');
 
-
-// RESPONSE FROM STRIPE
-// allowed_source_types: ["card"] (1)
-// amount: 4000
-// amount_details: {tip: {}}
-// automatic_payment_methods: null
-// canceled_at: null
-// cancellation_reason: null
-// capture_method: "automatic"
-// client_secret: "pi_3MJgaeHwot3YTHMG1xbJySNl_secret_o6SzNHeO2kgHf6qrwrQWfRmmb"
-// confirmation_method: "automatic"
-// created: 1672160724
-// currency: "gbp"
-// description: "MarGin – 50cl"
-// id: "pi_3MJgaeHwot3YTHMG1xbJySNl"
-// last_payment_error: null
-// livemode: false
-// next_action: null
-// next_source_action: null
-// object: "payment_intent"
-// payment_method: "pm_1MJgb4Hwot3YTHMGtDocuHSX"
-// payment_method_types: ["card"] (1)
-// processing: null
-// receipt_email: "hello@simonproudfoot.com"
-// setup_future_usage: null
-// shipping: null
-// source: null
-// status: "succeeded"
-
-
 exports.handler = async (event, context) => {
   const data = JSON.parse(event.body);
-  // const data = {
-  //   payment_method: "",
-  //   payment_method_title: "Direct Bank Transfer",
-  //   set_paid: true,
-  //   billing: {
-  //     first_name: "",
-  //     last_name: "",
-  //     address_1: "",
-  //     address_2: "",
-  //     city: "",
-  //     state: "",
-  //     postcode: "",
-  //     country: "UK",
-  //     email: "",
-  //     phone: ""
-  //   },
-  //   shipping: {
-  //     first_name: "",
-  //     last_name: "",
-  //     address_1: "",
-  //     address_2: "",
-  //     city: "",
-  //     state: "",
-  //     postcode: "",
-  //     country: ""
-  //   },
-  //   line_items: [
-  //     {
-  //       product_id: 93,
-  //       quantity: 2
-  //     },
-  //   ],
-   
-  // };
+
   console.log(data)
   // Extract the query string parameters
   const params = event.queryStringParameters;
