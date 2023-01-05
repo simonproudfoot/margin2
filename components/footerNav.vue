53a00d5a5fa3a7f11a651e15e964ab67726d5458
simonproudfoot <simonproudfoot1980@gmail.com>
2023-01-05 09:01:05 +0000

    Revert "new apple auth"
    This reverts commit c39774815873a93e8cd9fb3646881049b7ef7ff6.
    
    Reverted to branch before stripe changes

--- components/footerNav.vue
+++ components/footerNav.vue
@@ -3,12 +3,18 @@
     <span class="text-sm text-center text-white order-2 mt-6 md:mt-0 pt-3 md:pt-0 border-t md:border-0">©{{new Date().getFullYear()}} Margin London Ltd. All Rights Reserved.
     </span>
     <ul class="flex flex-col md:flex-row order-1 md:order-2 text-center mb-0 flex-wrap items-center md:text-left mt-3 text-sm text-white sm:mt-0">
+       
+       
         <li v-for="page in $store.state.nav.footerNav" :key="page.id" class="md:mr-4 hover:underline mr-8 mb-3 md:mb-0 md:mr-6">
             <nuxt-link v-if="page.type !== 'custom' && page.type_label=='Page'" :to="'/'+page.slug" v-html="page.title"></nuxt-link>
-            <nuxt-link class="" v-else-if="page.type !== 'custom' && page.type_label=='Post'" :to="'/blog/'+page.slug" v-html="page.title"></nuxt-link>
-            <a class="" v-else :href="page.url" target="_blank" v-html="page.title"></a>
+                    <nuxt-link class="" v-else-if="page.type !== 'custom' && page.type_label=='Post'" :to="'/blog/'+page.slug" v-html="page.title"></nuxt-link>
+                    <a class="" v-else :href="page.url" target="_blank" v-html="page.title"></a>
         </li>
     </ul>
+
+
+
+   
 </footer>
 </template>
 
